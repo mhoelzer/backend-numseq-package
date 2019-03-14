b19623cc6844a68fe785485251ac6e895f244711
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-14 12:50:48 -0400

    geo stuff should be done

--- numseq/geo.py
+++ numseq/geo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Numseq Assessment--Geometric Numbers"""
+"""
+Numseq Assessment--Geometric Numbers
+because we have to get in SHAPE
+"""
 
 __author__ = "mhoelzer"
 
